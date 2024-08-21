c71d00bc0622b638594b96c9d3d2719d0cfd37b3
msjimc <msjimc@leeds.ac.uk>
2024-08-21 16:45:34 +0100

    Add project files.

---
+++ Get_LOC/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Get_LOC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLOCList = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAnswers = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtLOCList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(557, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtLOCList
+            // 
+            this.txtLOCList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLOCList.Location = new System.Drawing.Point(56, 13);
+            this.txtLOCList.Multiline = true;
+            this.txtLOCList.Name = "txtLOCList";
+            this.txtLOCList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLOCList.Size = new System.Drawing.Size(495, 108);
+            this.txtLOCList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(476, 127);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "button1";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtAnswers);
+            this.groupBox2.Location = new System.Drawing.Point(12, 174);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(557, 264);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // txtAnswers
+            // 
+            this.txtAnswers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAnswers.Location = new System.Drawing.Point(52, 12);
+            this.txtAnswers.Multiline = true;
+            this.txtAnswers.Name = "txtAnswers";
+            this.txtAnswers.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtAnswers.Size = new System.Drawing.Size(495, 239);
+            this.txtAnswers.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 461);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLOCList;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAnswers;
+    }
+}
+
---
+++ Get_LOC/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Reflection;
+
+namespace Get_LOC
+{
+    public partial class Form1 : Form
+    {
+        WebClient client;
+        string url = "https://www.ncbi.nlm.nih.gov/gene/?term=";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtLOCList.Text = "SOX9\r\nLOC751814\r\nLOC610636\r\nLOC607095\r\nLOC484356\r\nLOC480441\r\nLOC479761\r\nLOC477539\r\nLOC403585\r\nLOC106557476\r\nLOC102155886\r\nLOC102155410\r\nLOC102153034\r\nLOC102152706\r\nLOC102152620\r\nLOC102151424\r\nLOC100856786\r\nLOC100855634\r\nLOC100855600\r\nLOC100686502\r\nLOC100686073\r\nLOC100684432\r\nLOC100683370\r\nLOC100049001\r\n";
+
+            client = new WebClient();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            string[] entries = txtLOCList.Lines;
+            txtAnswers.Clear();
+
+            foreach (string entry in entries)
+            {                
+                string term = entry.Trim().ToLower();
+                
+                if (string.IsNullOrEmpty(term) == false)
+                {
+                    if (term.StartsWith("loc") == true)
+                    {
+                        string answer = searchThisLOCTerm(term);
+                        if (answer != "Error")
+                        { txtAnswers.Text += entry + "\t" + answer + "\r\n"; }
+                        else
+                        { txtAnswers.Text += entry + "\t" + answer + "\r\n"; }
+                    }
+                    else
+                    {
+                        string answer = searchThis(term);
+                        if (answer != "Error")
+                        { txtAnswers.Text += entry + "\t" + answer + "\r\n"; }
+                        else
+                        { txtAnswers.Text += entry + "\t" + answer + "\r\n"; }
+                    }
+                    Application.DoEvents();
+                }
+            }
+
+
+
+            //string answer = "";
+            //string term = "LOC751814";
+            //term = "sox9";
+        }
+
+        private string searchThisLOCTerm(string term) 
+        {
+            string answer = "Error";
+
+            char[] endOfLineChar = { '\r', '\n', '<' };
+            
+            string thisURL = url + term;
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            string contents = getText(thisURL);
+
+            int index = contents.ToLower().IndexOf(">" + term.ToLower());
+            if (index > -1)
+            {
+                int endOfLine = contents.IndexOfAny(endOfLineChar, index + 10);
+                if (endOfLine > -1)
+                { 
+                    int startTextClipFrom = index + 1 + term.Length;
+                    answer = contents.Substring(startTextClipFrom, endOfLine - startTextClipFrom);
+                    if (answer.Contains("- Gene") == true)
+                    {
+                        int newEnd = answer.IndexOf("- Gene");
+                        if (newEnd > -1)
+                        { answer =  answer.Substring(0, newEnd); }
+                    }
+                    else if (string.IsNullOrEmpty(answer) == true)
+                    {
+                        index = contents.ToLower().IndexOf("<title>") + 7;
+                        endOfLine = contents.IndexOf("</title>", index);
+                        answer = contents.Substring(index, endOfLine-index);
+                        if (answer.Contains("- Gene") == true)
+                        {
+                            int newEnd = answer.IndexOf("- Gene");
+                            if (newEnd > -1)
+                            { answer = answer.Substring(0, newEnd); }
+                        }
+                    }
+                }
+            }
+
+            return answer.Trim();
+
+        }
+
+        private string searchThis(string term)
+        {
+            string answer = "Error";
+
+            char[] endOfLineChar = { '\r', '\n', '<' };
+
+            string thisURL = url + term;
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            string contents = getText(thisURL);
+
+            if (contents == "Error") { return answer; }
+
+            int startPoint = contents.IndexOf("https://www.ncbi.nlm.nih.gov/gene/") + 1;
+            int index = contents.ToLower().IndexOf(">" + term.ToLower(), startPoint);
+            if (index > -1)
+            {
+                int endOfLine = contents.IndexOfAny(endOfLineChar, index);
+                if (endOfLine > -1)
+                {
+                    int textClipStar = index + 3 + term.Length;
+                    answer = contents.Substring(textClipStar, endOfLine - textClipStar); 
+                }
+            }            
+
+            return answer;
+
+        }
+
+
+        private string getText(string urlToUse)
+        {
+            try
+            {
+                Stream data = client.OpenRead(urlToUse);
+                StreamReader sr = new StreamReader(data);
+                string fileText = sr.ReadToEnd();
+
+                data.Close();
+                sr.Close();
+
+                return fileText;
+            }
+            catch { return "Error"; }
+        }
+
+    }
+}
